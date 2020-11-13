30a07a2c2caf2916f2573f0451215fb000ee57a1
Xavier <xavier.carrera1990@gmail.com>
2020-11-12 19:39:35 -0600

    Test commit

---
+++ 1-top5.sql
@@ -0,0 +1,16 @@
+/* Select the first 5 registers */
+
+SELECT * 
+FROM platzi.alumnos
+FETCH FIRST 5 ROWS ONLY;
+
+SELECT *
+FROM platzi.alumnos
+LIMIT 5;
+
+SELECT * 
+FROM (
+	SELECT ROW_NUMBER() OVER() AS row_id, *
+	FROM platzi.alumnos
+) AS alumnos_with_row_num
+WHERE row_id <= 5;
